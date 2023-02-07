3f79c275a2c0e8e21e418b83daed092232529778
rotem <nevgauker@gmail.com>
2023-02-07 13:02:28 +0200

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Rotem Nevgauker on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
